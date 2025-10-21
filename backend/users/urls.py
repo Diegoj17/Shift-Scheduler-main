280217a3a7f81484044bb8b7c97f546fe1f3250b
Diego_jt <99993658+Diegoj17@users.noreply.github.com>
2025-10-21 14:55:10 -0500

    Update URL paths to include trailing slashes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,9 +3,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, LoginView, MeView, PasswordResetRequestView, PasswordResetConfirmView, AdminCreateUserView,AdminUserDetailView,AdminBlockUserView,AdminUserAccessView
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="auth-register"),
-    path("login", LoginView.as_view(), name="auth-login"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
+    path("register/", RegisterView.as_view(), name="auth-register"),
+    path("login/", LoginView.as_view(), name="auth-login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("me", MeView.as_view(), name="auth-me"),
     path("password/reset", PasswordResetRequestView.as_view(), name="password-reset-request"),
     path("password/reset/confirm", PasswordResetConfirmView.as_view(), name="password-reset-confirm"),
